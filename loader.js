261f19e046aa49fdd6fbb0fa0aa3bd861a34470b
ENOZ <enoz@enozedu.com>
2026-02-26 17:05:21 +0900

    Update runtime URL: https://roy-canberra-ent-barn.trycloudflare.com

--- loader.js
+++ loader.js
@@ -4,7 +4,7 @@
   var RUNTIME_URL = "https://raw.githubusercontent.com/enoz3/digitaledu-chat-loader/main/runtime.json";
   var RUNTIME_MIRROR_URL = "https://cdn.jsdelivr.net/gh/enoz3/digitaledu-chat-loader@main/runtime.json";
   var DEFAULT_BASE_URL = "https://roy-canberra-ent-barn.trycloudflare.com";
-  var DEFAULT_SECONDARY_BASE_URL = "https://engineering-moral-activation-zealand.trycloudflare.com";
+  var DEFAULT_SECONDARY_BASE_URL = "https://resolved-return-cute-employee.trycloudflare.com";
   var WIDGET_LOAD_TIMEOUT_MS = 8000;
 
   function getCurrentScript() {
